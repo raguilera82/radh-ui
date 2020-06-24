194fe05a9fc073ab214809202e26b440bfbc3d06
Ruben Aguilera <radhcode@gmail.com>
2020-06-24 22:55:39 +0200

    v2.3.3

--- src/components/radh-lazy-img/radh-lazy-img.stories.js
+++ src/components/radh-lazy-img/radh-lazy-img.stories.js
@@ -6,6 +6,7 @@ addDecorator(withA11y);
 
 storiesOf('radh-lazy-img', module)
     .add('With several load times', () => `
+    <radh-fluid-grid>
         <radh-lazy-img time="3500" img-src="/assets/imgs/dummy-on.jpeg"></radh-lazy-img>
         <radh-lazy-img time="1000" img-src="/assets/imgs/dummy-on.jpeg"></radh-lazy-img>
         <radh-lazy-img time="3000" img-src="/assets/imgs/dummy-on.jpeg"></radh-lazy-img>
@@ -16,4 +17,5 @@ storiesOf('radh-lazy-img', module)
         <radh-lazy-img time="2000" img-src="/assets/imgs/dummy-on.jpeg"></radh-lazy-img>
         <radh-lazy-img time="1000" img-src="/assets/imgs/dummy-on.jpeg"></radh-lazy-img>
         <radh-lazy-img time="1000" img-src="/assets/imgs/dummy-on.jpeg"></radh-lazy-img>    
+    </radh-fluid-grid>
     `, { notes });
